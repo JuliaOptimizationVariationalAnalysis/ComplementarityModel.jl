abc146b119c655dfc541cec1ac5937adb6d06200
Documenter.jl <documenter@juliadocs.github.io>
2021-11-06 14:41:12 +0000

    build based on 6495e19

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ComplementarityModel","category":"page"},{"location":"#ComplementarityModel","page":"Home","title":"ComplementarityModel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ComplementarityModel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ComplementarityModel]","category":"page"}]
+}
